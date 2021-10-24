6158e7b663a51b3e79ed5fccda19d483d9103529
littlebear0729 <littlebear0729@protonmail.com>
2021-10-24 17:16:47 +0800

    Use random filename

--- index.js
+++ index.js
@@ -34,19 +34,19 @@ bot.on('message', (msg) => {
         .then((newmsg) => {
           const newmsgId = newmsg.message_id
           const fileID = msg.sticker.file_id
-          const filename = msg.sticker.set_name + '_' + msg.sticker.file_unique_id + '.gif.1'
+          const filename = msg.sticker.set_name + '_' + msg.sticker.file_unique_id
           // download sticker using fileID and get link
           bot.getFileLink(fileID)
             .then((link) => {
-              downloadFile(link, 'tempTGS.tgs')
+              downloadFile(link, 'file/' + filename + '.tgs')
                 .then(() => {
                   // convert file from .tgs to .gif
-                  Convertor.convertFile('tempTGS.tgs')
+                  Convertor.convertFile('file/' + filename + '.tgs')
                     .then(() => {
                       bot.editMessageText('Sending...', { chat_id: chatId, message_id: newmsgId })
-                      const file = fs.createReadStream('tempTGS.tgs.gif')
+                      const file = fs.createReadStream('file/' + filename + '.tgs.gif')
                       // send file renaming it to .gif.1
-                      bot.sendDocument(chatId, file, { reply_to_message_id: messageId }, { filename: filename })
+                      bot.sendDocument(chatId, file, { reply_to_message_id: messageId }, { filename: filename + '.gif.1' })
                         .then(function () {
                           bot.deleteMessage(chatId, newmsgId)
                         })
@@ -59,19 +59,19 @@ bot.on('message', (msg) => {
         .then((newmsg) => {
           const newmsgId = newmsg.message_id
           const fileID = msg.sticker.file_id
-          const filename = msg.sticker.set_name + '_' + msg.sticker.file_unique_id + '.png'
+          const filename = msg.sticker.set_name + '_' + msg.sticker.file_unique_id
           bot.getFileLink(fileID)
             .then((link) => {
               // download sticker
-              downloadFile(link, 'tempSticker.webp')
+              downloadFile(link, 'file/' + filename + '.webp')
                 .then(function () {
                   // convert sticker from webp to .png using webp.dwebp
-                  webp.dwebp('tempSticker.webp', 'tempSticker.png', '-o', '-v')
+                  webp.dwebp('file/' + filename + '.webp', 'file/' + filename + '.png', '-o', '-v')
                     .then(function () {
                       bot.editMessageText('Sending...', { chat_id: chatId, message_id: newmsgId })
-                      const file = fs.createReadStream('tempSticker.png')
+                      const file = fs.createReadStream('file/' + filename + '.png')
                       // send file renaming to .png
-                      bot.sendDocument(chatId, file, { reply_to_message_id: messageId }, { filename: filename })
+                      bot.sendDocument(chatId, file, { reply_to_message_id: messageId }, { filename: filename + '.png' })
                         .then(function () {
                           bot.deleteMessage(chatId, newmsgId)
                         })
