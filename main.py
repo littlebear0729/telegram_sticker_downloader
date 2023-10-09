a69a9c155a85e6fda7b68645924cc2eacf28459f
Cyunrei <cyunrei@gmail.com>
2023-10-09 08:14:30 +0800

    refactor: remove local proxy

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def main(bot_token: str) -> None:
     print(admin, whitelist)
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(bot_token).proxy_url('http://localhost:1089').build()
+    application = Application.builder().token(bot_token).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
